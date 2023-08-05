8a01ffc36c2716bbe2150b35ee7a8d529a6450ce
sumanthrancho <140693554+sumanthrancho@users.noreply.github.com>
2023-08-05 13:39:18 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open('Diabetes-pred/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
 
 # creating a function for Prediction
